bb67e2fb0aa4a07166d23a5e58ab1d927122b4a1
Nemish Rajput <nemishrajput@Nemishs-MacBook-Air.local>
2025-06-23 17:44:31 +0530

    Initial commit for frontend

--- src/Constants.jsx
+++ src/Constants.jsx
@@ -1,6 +1,6 @@
 
 // const Origin = "https://apis-color-prediction-game.earnvern.com" ;
-const Origin = "✅ https://liquidity-app-initial.onrender.com";
+const Origin = "https://liquidity-app-initial.onrender.com";
 // const Origin = "http://192.168.1.9:4500";
 
 
